125e558879e6cb20a3d057ca46908580b1391439
KHS <ewoking777@gmail.com>
2023-03-05 01:01:05 +0900

    ch6 6-1,2번

---
+++ src/ch6_oop/Exercose6_2.java
@@ -0,0 +1,11 @@
+package ch6_oop;
+
+public class Exercose6_2 {
+    public static void main(String[] args) {
+        SutdaCard card1 = new SutdaCard(3, false);
+        SutdaCard card2 = new SutdaCard();
+
+        System.out.println(card1.info());
+        System.out.println(card2.info());
+    }
+}
---
+++ src/ch6_oop/SutdaCard.java
@@ -0,0 +1,26 @@
+package ch6_oop;
+
+public class SutdaCard {
+    int num;
+    boolean isKwang;
+
+    SutdaCard() {
+        this(1, true);
+    }
+
+    SutdaCard(int num, boolean isKwang) {
+        this.num = num;
+        this.isKwang = isKwang;
+    }
+
+    String info() {
+        num = this.num;
+        isKwang = this.isKwang;
+        if (isKwang==true) {
+            return num+"K";
+        } else {
+            return num + "";
+        }
+        // return num + ( isKwang ? "K" : "");
+    }
+}
