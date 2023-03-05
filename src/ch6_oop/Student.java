72d37c62b801caeeeffeff73248e12009b94988c
KHS <ewoking777@gmail.com>
2023-03-05 19:15:48 +0900

    ch6 6-3번

---
+++ src/ch6_oop/Student.java
@@ -0,0 +1,10 @@
+package ch6_oop;
+
+public class Student {
+    String name;    // 학생 이름
+    int ban;        // 반
+    int no;         // 번호
+    int kor;        // 국어점수
+    int eng;        // 영어점수
+    int math;       // 수학점수
+}
