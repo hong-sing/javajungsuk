8c2cd90e7ddb50ac6bbab3c59a3a21768d3a2317
KHS <ewoking777@gmail.com>
2023-03-05 20:19:31 +0900

    ch6 6-5번

--- src/ch6_oop/Student.java
+++ src/ch6_oop/Student.java
@@ -8,6 +8,17 @@ public class Student {
     int eng;        // 영어점수
     int math;       // 수학점수
 
+    Student() {}
+
+    Student(String name, int ban, int no, int kor, int eng, int math) {
+        this.name = name;
+        this.ban = ban;
+        this.no = no;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+    }
+
     int getTotal() {
         return this.kor + this.eng + this.math;
     }
@@ -25,5 +36,9 @@ public class Student {
             (int)(236 / 3f * 10 + 0.5) / 10 78 →
             (int)(236 / 3f * 10 + 0.5) / 10f 78.7
          */
+    }
+
+    String info() {
+        return name+","+ban+","+no+","+kor+","+eng+","+math+","+getTotal()+","+getAverage();
     }
 }
